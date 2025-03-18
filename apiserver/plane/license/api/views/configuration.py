1ccda1f5278c5f3744be784239b9fe3a906b3cb5
iB Arts Pvt Ltd <48272448+ibartsindia@users.noreply.github.com>
2025-03-18 05:44:45 +0530

    Update configuration.py
    Replaced Plane with ReyFlow

--- apiserver/plane/license/api/views/configuration.py
+++ apiserver/plane/license/api/views/configuration.py
@@ -86,8 +86,8 @@ class EmailCredentialCheckEndpoint(BaseAPIView):
             use_ssl=EMAIL_USE_SSL == "1",
         )
         # Prepare email details
-        subject = "Email Notification from Plane"
-        message = "This is a sample email notification sent from Plane application."
+        subject = "Email Notification from ReyFlow"
+        message = "This is a sample email notification sent from ReyFlow application."
         # Send the email
         try:
             msg = EmailMultiAlternatives(
